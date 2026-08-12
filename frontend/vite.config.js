f9d7373c61566e31bcbf96be69fdfa1a6f49249b
Esakki Durai <esakkidurai5600@gmail.com>
2026-08-12 15:55:26 +0530

    Update base path to use new repo name Portfolio_Generator_MERN_stack

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio_Generator_Backend/',
+  base: '/Portfolio_Generator_MERN_stack/',
 })
